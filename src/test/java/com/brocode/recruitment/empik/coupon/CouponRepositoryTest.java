5a522b4e34f873805849f6fce3b9870e2ddf0996
tomas <gandalf@lord-of-the-lines.com>
2025-07-07 11:04:15 +0200

    feat(test): Added coupon repository test

--- src/test/java/com/brocode/recruitment/empik/coupon/CouponRepositoryTest.java
+++ src/test/java/com/brocode/recruitment/empik/coupon/CouponRepositoryTest.java
@@ -1,4 +1,3 @@
-/*
 package com.brocode.recruitment.empik.coupon;
 
 import com.brocode.recruitment.empik.coupon.model.Coupon;
@@ -11,6 +10,7 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.jdbc.Sql;
 
 import java.time.LocalDateTime;
+import java.time.Month;
 import java.util.Optional;
 
 @ActiveProfiles("itest")
@@ -18,14 +18,46 @@ import java.util.Optional;
 class CouponRepositoryTest {
     @Autowired CouponRepository repository;
 
+    private final String correctUuid = "UUID-TEST-OK";
+    private final String wrongUuid = "UUID-TEST-NOK";
+    private final LocalDateTime now = LocalDateTime.now();
+    private final LocalDateTime oldDate = LocalDateTime.of(2024, Month.JANUARY, 1, 0, 0);
+    private final String correctLocale = "US";
+    private final String wrongLocalee = "PL";
+
     @Test
     @Sql("initial_data.sql")
     void couponFound() {
-        repository.findAll().forEach(System.out::println);
-        String uuid = "UUID-TEST-OK";
-        Optional<Coupon> couponByUUID = repository.findCouponByUuidAndLocaleAndCreationDateBefore(uuid, "US",
-                LocalDateTime.now());
+        Optional<Coupon> couponByUUID = repository.findCouponByUuidAndLocaleAndCreationDateBefore(correctUuid, correctLocale,
+                now);
+
         Assertions.assertThat(couponByUUID).isPresent();
     }
+
+    @Test
+    @Sql("initial_data.sql")
+    void notFound_wrongUuid() {
+        Optional<Coupon> couponByUUID = repository.findCouponByUuidAndLocaleAndCreationDateBefore(wrongUuid, correctLocale,
+                now);
+
+        Assertions.assertThat(couponByUUID).isEmpty();
+    }
+
+    @Test
+    @Sql("initial_data.sql")
+    void notFound_wrongLocale() {
+        Optional<Coupon> couponByUUID = repository.findCouponByUuidAndLocaleAndCreationDateBefore(correctUuid, wrongLocalee,
+                now);
+
+        Assertions.assertThat(couponByUUID).isEmpty();
+    }
+
+    @Test
+    @Sql("initial_data.sql")
+    void notFound_wrongDate() {
+        Optional<Coupon> couponByUUID = repository.findCouponByUuidAndLocaleAndCreationDateBefore(correctUuid, correctLocale,
+                oldDate);
+
+        Assertions.assertThat(couponByUUID).isEmpty();
+    }
 }
-*/
